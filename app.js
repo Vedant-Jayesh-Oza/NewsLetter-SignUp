bb7a9362d098af7f609e08169eabb31bca627c8b
Vedant-Jayesh-Oza <vedantoza1313@gmail.com>
2022-06-28 15:16:37 +0530

    Posted Data to Mailchimp server via API integration.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser =  require("body-parser");
 const request = require("request");
-
+const https = require("https");
 const app = express();
 
 app.use(express.static("public"));
@@ -12,11 +12,40 @@ app.get("/",function(req, res){
 });
 
 app.post("/", function(req, res){
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;  
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;  
+  
+  const data = {
+    members: [
+        {
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }
+    ]
+  };
+  
+  const jsonData = JSON.stringify(data);
+  
+  const url = "https://us8.api.mailchimp.com/3.0/lists/279ea1365d";
 
-  console.log(firstName, lastName, email);
+  const options = {
+    method: "POST",
+    auth: "vedant1:9570cf905d7893d50f783ea8df767963-us8"
+  }
+
+  const request = https.request(url, options, function(response) {
+    response.on("data", function(data){
+        console.log(JSON.parse(data));
+    })
+  })
+  
+  request.write(jsonData);
+  request.end();
 
 });
 
@@ -25,3 +54,13 @@ app.post("/", function(req, res){
 app.listen(3000, function(){
     console.log("server is running on 3000");
 })
+
+
+
+
+//API key 
+//9570cf905d7893d50f783ea8df767963-us8
+
+
+//List ID
+//279ea1365d
