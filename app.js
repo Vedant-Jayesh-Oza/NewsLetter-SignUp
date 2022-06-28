02ef4bd6b23548c866295d86d6c925b285ae06dd
Vedant-Jayesh-Oza <vedantoza1313@gmail.com>
2022-06-28 14:13:36 +0530

    signup page established on the server

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req, res){
+    res.sendFile(__dirname+"/signup.html")
+});
+
+app.post("/", function(req, res){
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;  
+
+  console.log(firstName, lastName, email);
+
+});
+
+
+
 app.listen(3000, function(){
     console.log("server is running on 3000");
 })
