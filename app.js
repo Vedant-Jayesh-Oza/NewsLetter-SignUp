883c3051b8f809905975b746be2296bcadbd6c7e
Vedant-Jayesh-Oza <vedantoza1313@gmail.com>
2022-06-28 16:01:12 +0530

    success and failure page added

--- app.js
+++ app.js
@@ -39,6 +39,14 @@ app.post("/", function(req, res){
   }
 
   const request = https.request(url, options, function(response) {
+
+    if(response.statusCode === 200){
+        res.sendFile(__dirname + "/success.html");
+    } else {
+        res.sendFile(__dirname + "/failure.html");
+        
+    }
+
     response.on("data", function(data){
         console.log(JSON.parse(data));
     })
@@ -48,6 +56,10 @@ app.post("/", function(req, res){
   request.end();
 
 });
+
+app.post("/failure", function(req, res) {
+  res.redirect("/")
+})
 
 
 
